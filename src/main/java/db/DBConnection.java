1d7adbb57a589b24f900cb6376a7614d344e100d
ghamza-dev <elgassaihamzam54@gmail.com>
2022-10-03 09:39:03 +0200

    Create database connection and admin service

--- src/main/java/db/DBConnection.java
+++ src/main/java/db/DBConnection.java
@@ -2,43 +2,22 @@ package db;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.Statement;
 
 public class DBConnection {
-    private final static String URL = "jdbc:mysql://localhost:3306/ma-cnss";
-    private final static String USER = "root";
-    private final static String PASS = "";
+    private final String URL = "jdbc:mysql://localhost:3306/ma-cnss";
+    private final String USER = "root";
+    private final String PASS = "";
 
-    private static Connection con;
-    private static Statement stmt;
+    private Connection con;
 
-    private static DBConnection instance;
-
-    private DBConnection() {
-        setConnection();
-        setStatement();
-    }
-
-    public static DBConnection getInstance() {
-        if (instance == null) {
-            instance = new DBConnection();
-        }
-        return instance;
-    }
-
-    private static void setConnection() {
+    public DBConnection() {
         try {
-            con = DriverManager.getConnection(URL, USER, PASS);
+            this.con = DriverManager.getConnection(URL, USER, PASS);
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
     }
-
-    private static void setStatement() {
-        try {
-            stmt = con.createStatement();
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
+    public Connection getConnection() {
+        return this.con;
     }
 }
