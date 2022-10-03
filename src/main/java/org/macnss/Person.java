32441df42e212e8f5fb41b900147d3633f46c78c
ghamza-dev <elgassaihamzam54@gmail.com>
2022-10-03 09:40:59 +0200

    Admin and person classes

--- src/main/java/org/macnss/Person.java
+++ src/main/java/org/macnss/Person.java
@@ -1,24 +1,25 @@
 package org.macnss;
 
-public class Person{
-    private String firstName;
-    private String lastName;
+public class Person {
+
+    private int id;
+    private String username;
     private String email;
     private String password;
 
-    public Person(String firstName, String lastName, String email, String password) {
-        this.firstName = firstName;
-        this.lastName = lastName;
+    public Person() {
+
+    }
+
+    public Person(int id,String username,String email, String password) {
+        this.id = id;
+        this.username = username;
         this.email = email;
         this.password = password;
     }
 
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
+    public String getUsername() {
+        return username;
     }
 
     public String getEmail() {
