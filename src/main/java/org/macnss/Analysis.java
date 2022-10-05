338701545f2425762a9f99ee987b89faebef60f6
benjarmoun <93975590+benjarmoun@users.noreply.github.com>
2022-10-05 10:53:04 +0100

    create analysis class

---
+++ src/main/java/org/macnss/Analysis.java
@@ -0,0 +1,18 @@
+package org.macnss;
+
+public class Analysis extends Document{
+    private float percentage;
+
+    public Analysis(int id, String name, float percentage) {
+        super(id, name);
+        this.percentage = percentage;
+    }
+
+    public float getPercentage() {
+        return percentage;
+    }
+
+    public void setPercentage(float percentage) {
+        this.percentage = percentage;
+    }
+}
